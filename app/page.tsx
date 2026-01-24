6d0c3fe571cf837f653296179e403e1e3aff706e
Joshua Goldberg <mail@joshgold.design>
2026-01-24 10:49:01 +0200

    Replace multiple sections with a single BentoPortfolio component for a streamlined homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
-import Header from "@/components/Header";
-import HeroSection from "@/components/HeroSection";
-import WorkSection from "@/components/WorkSection";
-import AboutSection from "@/components/AboutSection";
-import ContactSection from "@/components/ContactSection";
-import Footer from "@/components/Footer";
+import BentoPortfolio from "@/components/BentoPortfolio";
 
 export default function Home() {
   return (
     <main className="relative z-10">
-      <Header />
-      <HeroSection />
-      <WorkSection />
-      <AboutSection />
-      <ContactSection />
-      <Footer />
+      <BentoPortfolio />
     </main>
   );
 }
